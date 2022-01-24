78543385034e6a5f131b1b3a9f83c3b9561b815c
Sangyups <feels4637@gmail.com>
2022-01-24 23:06:51 +0900

    add test1

---
+++ src/test1.cpp
@@ -0,0 +1,25 @@
+  #include <bits/stdc++.h>
+
+  #define endl '\n'
+  #define print(x) cout << x << endl
+  #define clearArray(arr, num) memset(arr, num, sizeof(arr))
+  #define clear2DArray(arr, num, row)     for (int i = 0; i < row; i++) {         clearArray(arr[i], num);            }
+  #define pii pair<int, int>
+  #define INF 987654321
+  typedef long long ll;
+  using namespace std;
+
+  #define MAX 9
+  int N;
+
+  void solution() {
+
+  }
+
+  int main(int argc, const char* argv[]) {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+
+    
+    solution();
+  }
