7e8c107ff5e24414d50656a6ba44c27fc38af4e3
Sangyups <feels4637@gmail.com>
2022-05-24 00:44:54 +0900

    백준_19551_태상이의훈련소생활.cpp

---
+++ src/백준/19551_태상이의훈련소생활.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <tuple>
+
+#define endl '\n'
+#define pii pair<int, int>
+#define tiii tuple<int, int, int>
+
+typedef long long ll;
+
+using namespace std;
+
+#define MAX 100002
+
+int N, M;
+int height[MAX];
+int d_height[MAX];
+tiii instruction[MAX];
+
+void solution() {
+  for (auto inst: instruction) {
+    auto[a, b, k] = inst;
+    d_height[a] += k;
+    d_height[b + 1] -= k;
+  }
+  for (int i = 1; i <= N; i++) {
+    height[i] += d_height[i];
+    cout << height[i] << ' ';
+    d_height[i + 1] += d_height[i];
+  }
+}
+
+int main(int argc, const char *argv[]) {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+
+  cin >> N >> M;
+
+  for (int i = 1; i <= N; i++) {
+    cin >> height[i];
+  }
+
+  for (int i = 0; i < M; i++) {
+    int a, b, k;
+    cin >> a >> b >> k;
+    instruction[i] = make_tuple(a, b, k);
+  }
+
+  solution();
+}
