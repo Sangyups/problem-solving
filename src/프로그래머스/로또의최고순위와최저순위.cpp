fbd2d415cf0a4822f9c37bb50e45fd351182acf0
Sangyups <feels4637@gmail.com>
2022-05-24 11:00:05 +0900

    프로그래머스_로또의최고순위와최저순위.cpp

---
+++ src/프로그래머스/로또의최고순위와최저순위.cpp
@@ -0,0 +1,35 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <map>
+
+using namespace std;
+
+map<int, int> m = {
+    {6, 1},
+    {5, 2},
+    {4, 3},
+    {3, 4},
+    {2, 5},
+    {1, 6},
+    {0, 6}
+};
+
+vector<int> solution(vector<int> lottos, vector<int> win_nums) {
+  vector<int> answer;
+  int zeros = 0;
+  int correct = 0;
+  for (auto num: lottos) {
+    if (!num) {
+      zeros++;
+      continue;
+    }
+    int idx = distance(win_nums.begin(), find(win_nums.begin(), win_nums.end(), num));
+    if (idx < win_nums.size()) correct++;
+  }
+
+  answer.push_back(m[correct + zeros]);
+  answer.push_back(m[correct]);
+
+  return answer;
+}
