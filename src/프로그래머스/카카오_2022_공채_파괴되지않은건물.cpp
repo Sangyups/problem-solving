9f52a7a6c27dafa66093190206934234b76269e2
Sangyups <feels4637@gmail.com>
2022-05-24 10:44:31 +0900

    카카오_2022_공채_파괴되지않은건물.cpp

--- src/프로그래머스/카카오_2022_공채_파괴되지않은건물.cpp
+++ src/프로그래머스/카카오_2022_공채_파괴되지않은건물.cpp
@@ -17,19 +17,10 @@ int solution(vector<vector<int>> board, vector<vector<int>> skill) {
   }
 
   for (int i = 0; i < board.size(); i++) {
-    for (int j = 0; j < board[0].size() - 1; j++) {
-      damage[i][j + 1] += damage[i][j];
-    }
-  }
-
-  for (int i = 0; i < board.size() - 1; i++) {
     for (int j = 0; j < board[0].size(); j++) {
-      damage[i + 1][j] += damage[i][j];
-    }
-  }
-
-  for (int i = 0; i < board.size(); i++) {
-    for (int j = 0; j < board[0].size(); j++) {
+      if (i == 0) damage[i][j] += damage[i][j - 1];
+      else if (j == 0) damage[i][j] += damage[i - 1][j];
+      else damage[i][j] += damage[i - 1][j] + damage[i][j - 1] - damage[i - 1][j - 1];
       if (board[i][j] + damage[i][j] > 0) answer++;
     }
   }
