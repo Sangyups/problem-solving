07f9afdeb0a0413a2cbe6df73c2ab9b6657de4a2
Sangyups <feels4637@gmail.com>
2022-06-01 01:34:15 +0900

    백준_23758_중앙값제거.cpp

---
+++ src/백준/23758_중앙값제거.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+#define endl '\n'
+#define pii pair<int, int>
+
+typedef long long ll;
+
+using namespace std;
+
+#define MAX 2000001
+
+int N;
+int arr[MAX];
+
+void solution() {
+  int cnt = 1;
+
+  for (int i = 1; i <= (N + 1) / 2; ++i) {
+    cnt += int(log2(arr[i]));
+  }
+  cout << cnt << endl;
+}
+
+int main(int argc, const char *argv[]) {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+
+  cin >> N;
+
+  for (int i = 1; i <= N; ++i) {
+    cin >> arr[i];
+  }
+
+  sort(arr + 1, arr + 1 + N);
+
+  solution();
+}
