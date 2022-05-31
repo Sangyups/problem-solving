df2e6c6d4eb9c6880246138f7d0af4d7b3afad3d
Sangyups <feels4637@gmail.com>
2022-05-31 20:24:14 +0900

    백준_1676_팩토리얼0의개수.cpp

---
+++ src/백준/1676_팩토리얼0의개수.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+#define endl '\n'
+#define pii pair<int, int>
+
+typedef long long ll;
+
+using namespace std;
+
+int N;
+
+void solution() {
+  int cnt = 0;
+  for (int i = 1; i <= N; ++i) {
+    int temp = i;
+    while (temp % 5 == 0 && temp > 0) {
+      ++cnt;
+      temp /= 5;
+    }
+  }
+  cout << cnt;
+}
+
+int main(int argc, const char *argv[]) {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+
+  cin >> N;
+
+  solution();
+}
