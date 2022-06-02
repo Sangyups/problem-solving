d598aa94e953f23bcf89b44e96e326bdbb6830c9
Sangyups <feels4637@gmail.com>
2022-06-02 19:48:23 +0900

    백준_1541_잃어버린괄호.cpp

---
+++ src/백준/1541_잃어버린괄호.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+#define endl '\n'
+#define pii pair<int, int>
+
+typedef long long ll;
+
+using namespace std;
+
+string equation;
+int hasMinus;
+
+void solution() {
+  int result = 0;
+  string num = "";
+  for (int i = 0; i <= equation.size(); ++i) {
+    if (equation[i] == '-' or equation[i] == '+' or i == equation.size()) {
+      if (hasMinus) result -= stoi(num);
+      else result += stoi(num);
+      if (equation[i] == '-') {
+        hasMinus = 1;
+      }
+      num = "";
+    } else {
+      num += equation[i];
+    }
+  }
+  cout << result;
+}
+
+int main(int argc, const char *argv[]) {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+
+  cin >> equation;
+
+  solution();
+}
