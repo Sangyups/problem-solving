16ffd9a490e9c7b4ed9bd5bbc22d9dbd10cfd215
Sangyups <feels4637@gmail.com>
2022-06-03 13:17:17 +0900

    백준_1992_쿼드트리.cpp

---
+++ src/백준/1992_쿼드트리.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+#define endl '\n'
+#define pii pair<int, int>
+
+typedef long long ll;
+
+using namespace std;
+
+#define MAX 70
+
+int N;
+int mat[MAX][MAX];
+
+string solution(int sx, int sy, int size) {
+  int temp = mat[sx][sy];
+  string ans = "";
+  for (int i = 0; i < size; ++i) {
+    for (int j = 0; j < size; ++j) {
+      if (mat[sx + i][sy + j] != temp) {
+        ans += "(";
+        for (int k = 0; k < 2; ++k) {
+          for (int l = 0; l < 2; ++l) {
+            ans += solution(sx + (size / 2) * k, sy + (size / 2) * l, size / 2);
+          }
+        }
+        ans += ")";
+        return ans;
+      }
+    }
+  }
+  return to_string(temp);
+}
+
+int main(int argc, const char *argv[]) {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+
+  cin >> N;
+  string s;
+
+  for (int i = 0; i < N; ++i) {
+    cin >> s;
+    for (int j = 0; j < s.size(); ++j) {
+      mat[i][j] = s[j] - 48;
+    }
+  }
+
+  cout << solution(0, 0, N);
+}
