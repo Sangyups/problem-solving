b607cc99b8e5d0e997bd2a1440efa8beed737702
Sangyups <feels4637@gmail.com>
2022-06-04 21:45:20 +0900

    백준_9375_패션왕신해빈.cpp

---
+++ src/백준/9375_패션왕신해빈.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+
+#define endl '\n'
+#define pii pair<int, int>
+
+typedef long long ll;
+
+using namespace std;
+
+int n;
+map<string, int> clothes;
+
+void solution() {
+  int answer = 1;
+  for (auto cloth : clothes) {
+    answer *= (cloth.second + 1);
+  }
+  cout << answer - 1 << endl;
+}
+
+int main(int argc, const char *argv[]) {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  int T;
+  cin >> T;
+
+  while (T--) {
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+      string cloth, type;
+      cin >> cloth >> type;
+      clothes[type]++;
+    }
+    solution();
+    clothes.clear();
+  }
+}
