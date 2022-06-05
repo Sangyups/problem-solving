38251616c551a9d7370f0fe8021c4df81e698271
Sangyups <feels4637@gmail.com>
2022-06-06 00:53:28 +0900

    백준_6064_카잉달력.cpp

---
+++ src/백준/6064_카잉달력.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+#define endl '\n'
+#define pii pair<int, int>
+
+typedef long long ll;
+
+using namespace std;
+
+ll M, N;
+ll x, y;
+
+ll gcd(ll a, ll b) {
+  while (b != 0) {
+    ll r = a % b;
+    a = b;
+    b = r;
+  }
+  return a;
+}
+
+ll lcm(ll a, ll b) {
+  return a * b / gcd(a, b);
+}
+
+void solution() {
+  ll limit = lcm(M, N);
+  if (x == M) x = 0;
+  for (int i = 0;; ++i) {
+    ll index = y + N * i;
+    if (index > limit) break;
+    if (index % M == x) {
+      cout << index << endl;
+      return;
+    }
+  }
+  cout << -1 << endl;
+}
+
+int main(int argc, const char *argv[]) {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+
+  int T;
+  cin >> T;
+
+  while (T--) {
+    cin >> M >> N >> x >> y;
+    solution();
+  }
+
+}
