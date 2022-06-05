aa798889b35138af5fac53637547479a084ffef8
Sangyups <feels4637@gmail.com>
2022-06-05 10:05:30 +0900

    백준_9461_파도반수열.cpp

---
+++ src/백준/9461_파도반수열.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+
+#define endl '\n'
+#define pii pair<int, int>
+
+typedef long long ll;
+
+using namespace std;
+
+#define MAX 105
+
+int n;
+ll arr[MAX] = {1, 1, 1, 2, 2, 3, 4, 5, 7, 9, 12};
+
+ll solution(int n) {
+  if (arr[n]) return arr[n];
+  arr[n] = solution(n - 1) + solution(n - 5);
+  return arr[n];
+}
+
+int main(int argc, const char *argv[]) {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+
+  int T;
+
+  cin >> T;
+
+  while (T--) {
+    cin >> n;
+    cout << solution(n - 1) << endl;
+  }
+}
