709502c3b24c47e7a5f78663803b7eba6e4af892
Sangyups <feels4637@gmail.com>
2022-06-26 22:58:04 +0900

    leetcode_22_generate_parentheses

---
+++ src/leetcode/22_generate_parentheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+ public:
+  vector<string> answer;
+  void solution(int start, int end, string s) {
+    if (start == 0 and end == 0) {
+      answer.push_back(s);
+      return;
+    }
+
+    if (start > 0) {
+      solution(start - 1, end, s + "(");
+    }
+    if (end > 0 and end > start) {
+      solution(start, end - 1, s + ")");
+    }
+  }
+
+  vector<string> generateParenthesis(int n) {
+    solution(n, n, "");
+
+    return answer;
+  }
+};
