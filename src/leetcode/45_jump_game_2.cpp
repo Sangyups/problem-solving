081da370ef31a53803040d8c086ea75fee1be9f7
Sangyups <feels4637@gmail.com>
2022-06-29 00:47:58 +0900

    leetcode_45_jump_game_2

---
+++ src/leetcode/45_jump_game_2.cpp
@@ -0,0 +1,18 @@
+class Solution {
+ public:
+  int N;
+  int jump(vector<int> &nums) {
+    N = nums.size();
+    int cnt = 0, curr_max = 0, next_max = 0;
+    for (int i = 0; i < N; ++i) {
+      next_max = max(i + nums[i], next_max);
+
+      if (i == curr_max and i != N - 1) {
+        cnt++;
+        curr_max = next_max;
+      }
+    }
+
+    return cnt;
+  }
+};
