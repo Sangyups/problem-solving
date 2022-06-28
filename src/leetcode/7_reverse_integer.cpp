dce89c58a1ee4f901a55947a63072528c0b90577
Sangyups <feels4637@gmail.com>
2022-06-28 20:41:11 +0900

    leetcode_7_reverse_integer

---
+++ src/leetcode/7_reverse_integer.cpp
@@ -0,0 +1,18 @@
+class Solution {
+ public:
+  int reverse(int x) {
+    int answer = 0;
+
+    while (x != 0) {
+      if ((answer == 214748364 and x % 10 > 7) or answer > 214748364) {
+        return 0;
+      }
+      if ((answer == -214748364 and x % 10 < -8) or answer < -214748364) {
+        return 0;
+      }
+      answer = answer * 10 + x % 10;
+      x /= 10;
+    }
+    return answer;
+  }
+};
