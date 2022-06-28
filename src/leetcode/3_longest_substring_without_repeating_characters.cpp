c99442738c599c740a574469dc308701e59e3add
Sangyups <feels4637@gmail.com>
2022-06-28 19:31:06 +0900

    leetcode_3_longest_substring_without_repeating_characters

---
+++ src/leetcode/3_longest_substring_without_repeating_characters.cpp
@@ -0,0 +1,22 @@
+#define MAX 50005
+
+class Solution {
+ public:
+  int dp[MAX];
+  unordered_map<char, int> m;
+  int lengthOfLongestSubstring(string s) {
+    int answer = 0;
+
+    for (int i = 0; i < s.size(); ++i) {
+      if (m.count(s[i]) == 0) {
+        m[s[i]] = i;
+        dp[i + 1] = dp[i] + 1;
+      } else {
+        dp[i + 1] = min(i - m[s[i]], dp[i] + 1);
+        m[s[i]] = i;
+      }
+      answer = max(dp[i + 1], answer);
+    }
+    return answer;
+  }
+};
