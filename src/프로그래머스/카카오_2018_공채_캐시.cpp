b25ba00ab71d633dee4237c58d1eb27ef869ac4c
Sangyups <feels4637@gmail.com>
2022-07-01 20:28:37 +0900

    프로그래머스_카카오_2018_공채_캐시.cpp

---
+++ src/프로그래머스/카카오_2018_공채_캐시.cpp
@@ -0,0 +1,39 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+#define MAX 35
+
+pair<string, int> cache[MAX];
+
+int solution(int cacheSize, vector<string> cities) {
+  int answer = 0;
+
+  for (int i = 0; i < cities.size(); ++i) {
+    string city = cities[i];
+    transform(city.begin(), city.end(), city.begin(), ::tolower);
+    int flag = 0;
+    int mn = 1987654321;
+    int min_index = 0;
+    for (int j = 0; j < cacheSize; ++j) {
+      if (cache[j].first == city) {
+        answer += 1;
+        cache[j].second = i + 1;
+        flag = 1;
+        break;
+      }
+      if (mn > cache[j].second) {
+        mn = cache[j].second;
+        min_index = j;
+      }
+    }
+    if (flag) continue;
+    cache[min_index] = {city, i + 1};
+    answer += 5;
+  }
+
+  return answer;
+}
