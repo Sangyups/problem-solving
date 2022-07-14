5859ac7a719e1ac363574830f792dc739ce91d8e
Sangyups <feels4637@gmail.com>
2022-07-14 22:40:10 +0900

    codeforces_1703_G_Good_Key_Bad_Key.cpp

---
+++ src/codeforces/1703_G_Good_Key_Bad_Key.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+#define endl '\n'
+#define pii pair<int, int>
+
+typedef long long ll;
+
+using namespace std;
+
+int T;
+int N, K;
+vector<int> chests;
+
+void preproc() {
+  chests.clear();
+}
+
+void solution() {
+  ll temp = 0;
+  for (int i = 0; i < min(N, 31); ++i) {
+    int val = chests[i];
+    val >>= i + 1;
+    temp += val;
+  }
+  ll ans = temp;
+  ll sum = 0;
+  for (int i = 0; i < N; ++i) {
+    sum += chests[i] - K;
+    temp = sum;
+    for (int j = i + 1; j < min(N, i + 32); ++j) {
+      int val = chests[j];
+      val >>= j - i;
+      temp += val;
+    }
+    ans = max(ans, temp);
+  }
+
+  cout << ans << endl;
+}
+
+int main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+
+  cin >> T;
+  while (T--) {
+    cin >> N >> K;
+    preproc();
+
+    for (int i = 0; i < N; ++i) {
+      int n;
+      cin >> n;
+      chests.push_back(n);
+    }
+    solution();
+  }
+}
