8ab06a5eb5aeb14342b705132063b840c765afd1
Sangyups <feels4637@gmail.com>
2022-07-15 23:11:21 +0900

    백준_2109_순회공연.cpp

---
+++ src/백준/2109_순회공연.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+#define endl '\n'
+#define pii pair<int, int>
+
+typedef long long ll;
+
+using namespace std;
+
+#define MAX 10005
+
+int N;
+pii arr[MAX];
+int schedule[MAX];
+
+void preproc() {
+  sort(arr, arr + N, greater<>());
+}
+
+void solution() {
+  int sum = 0;
+  for (int i = 0; i < N; ++i) {
+    auto [pay, day] = arr[i];
+    while (day >= 1 && schedule[day]) day--;
+    if (day >= 1) {
+      schedule[day] = pay;
+      sum += pay;
+    }
+  }
+  cout << sum << endl;
+}
+
+int main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+
+  cin >> N;
+
+  for (int i = 0; i < N; ++i) {
+    int a, b;
+    cin >> a >> b;
+    arr[i] = {a, b};
+  }
+
+  preproc();
+  solution();
+}
