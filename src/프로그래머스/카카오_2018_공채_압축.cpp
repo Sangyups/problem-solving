822bb6564f24877bffaaf70453991d9a915b88b4
Sangyups <feels4637@gmail.com>
2022-07-18 20:01:32 +0900

    프로그래머스_카카오_2018_공채_압축.cpp

---
+++ src/프로그래머스/카카오_2018_공채_압축.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+vector<int> solution(string msg) {
+  vector<int> answer;
+  int latest_num = 0;
+  map<string, int> m;
+  for (int i = 1; i <= 26; ++i) {
+    string temp = "";
+    temp += 'A' + i - 1;
+    m.insert({temp, i});
+    latest_num++;
+  }
+
+  for (int i = 0; i < msg.size(); ++i) {
+    string input = {msg[i]};
+    while (i < msg.size() - 1 && m.find(input + msg[i + 1]) != m.end()) {
+      input += msg[++i];
+    }
+    answer.push_back(m[input]);
+    if (i < msg.size() - 1) {
+      input += msg[i + 1];
+      m.insert({input, ++latest_num});
+    }
+  }
+  return answer;
+}
