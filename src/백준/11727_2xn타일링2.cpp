1cbadadffcb5f846ee600bfa90714ea490cdd058
Sangyups <feels4637@gmail.com>
2022-07-25 15:44:37 +0900

    백준_11727_2xn타일링2.cpp

---
+++ src/백준/11727_2xn타일링2.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+#define endl '\n'
+#define pii pair<int, int>
+
+typedef long long ll;
+
+using namespace std;
+
+#define MAX (1000+5)
+
+int N;
+int arr[MAX];
+
+void clean_up() {
+}
+
+void pre_proc() {
+  arr[1] = 1;
+  arr[2] = 3;
+}
+
+void solution() {
+  for (int i = 3; i <= N; ++i) {
+    arr[i] = (arr[i - 1] + arr[i - 2] * 2) % 10007;
+  }
+  cout << arr[N];
+}
+
+int main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+
+  cin >> N;
+
+  clean_up();
+  pre_proc();
+  solution();
+}
