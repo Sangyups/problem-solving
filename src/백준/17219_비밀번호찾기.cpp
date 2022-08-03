f90209ccfa4f7b5f05338d418f4c45bd4c25e3e1
Sangyups <feels4637@gmail.com>
2022-08-03 22:08:20 +0900

    백준_17219_비밀번호찾기.cpp

---
+++ src/백준/17219_비밀번호찾기.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+#define endl '\n'
+#define pii pair<int, int>
+
+typedef long long ll;
+
+using namespace std;
+
+int N, M;
+
+map<string, string> pw_list;
+
+void clean_up() {}
+
+void pre_proc() {}
+
+void solution() {
+  for (int i = 0; i < M; ++i) {
+    string site;
+    cin >> site;
+    cout << pw_list[site] << endl;
+  }
+}
+
+int main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+
+  cin >> N >> M;
+
+  for (int i = 0; i < N; ++i) {
+    string a, b;
+    cin >> a >> b;
+    pw_list.insert({a, b});
+  }
+
+  clean_up();
+  pre_proc();
+  solution();
+}
