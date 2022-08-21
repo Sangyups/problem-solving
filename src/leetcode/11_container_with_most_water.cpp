97de0c4b9782f792ee9b45030904f79ecfd26183
Sangyups <feels4637@gmail.com>
2022-08-21 23:11:33 +0900

    leetcode_11_container_with_most_water.cpp

---
+++ src/leetcode/11_container_with_most_water.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+  int maxArea(vector<int> &height) {
+    int ans = 0;
+    int s = 0, e = height.size() - 1;
+
+    while (s < e) {
+      int temp = min(height[s], height[e]) * (e - s);
+      ans = max(temp, ans);
+      if (height[s] < height[e]) ++s;
+      else --e;
+    }
+
+    return ans;
+  }
+};
