abdd92acd71dbd46016561bf23f82f4edffa4f94
Sangyups <feels4637@gmail.com>
2023-03-15 01:11:55 +0900

    leetcode_129_sum_root_to_leaf_numbers.cpp

---
+++ src/leetcode/129_sum_root_to_leaf_numbers.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+#define endl '\n'
+
+using namespace std;
+using pii = pair<int, int>;
+using ll = long long;
+
+void clean_up() {
+}
+
+void pre_proc() {
+}
+
+void solution() {
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+
+    clean_up();
+    pre_proc();
+    solution();
+}
