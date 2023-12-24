7d6dc22faf9e2d3d504d8ad8e19115b334e857f1
Sangyups <feels4637@gmail.com>
2023-12-24 12:23:41 +0900

    fix git push logic regardless of language

--- git_push.sh
+++ git_push.sh
@@ -14,6 +14,17 @@ done
 number='2'
 while [ $number != '1' ]
 do
+  echo "언어 종류를 입력해주세요."
+  read lang
+
+  echo "$lang 이(가) 맞습니까?"
+  echo "[1: 예 / 2: 아니오]"
+  read number
+done
+
+number='2'
+while [ $number != '1' ]
+do
   echo "출처 사이트를 입력해주세요."
   read site
 
@@ -22,12 +33,12 @@ do
   read number
 done
 
-mkdir -p ./src/${site} && cp ./main.go ./src/${site}/${prob}.go
-echo "src/${site}/${prob}.go에 저장되었습니다."
-git add ./src/${site}/${prob}.go
-git commit -m "${site}_${prob}.go"
+mkdir -p ./src/${site} && cp ./main.${lang} ./src/${site}/${prob}.${lang}
+echo "src/${site}/${prob}.${lang}에 저장되었습니다."
+git add ./src/${site}/${prob}.${lang}
+git commit -m "${site}_${prob}.${lang}"
 git push origin main
-echo "${site}/${prob}.go 파일 커밋이 완료되었습니다."
+echo "${site}/${prob}.${lang} 파일 커밋이 완료되었습니다."
 
 sh init.sh
 
