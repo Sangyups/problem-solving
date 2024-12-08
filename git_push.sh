9e9ac7986f5210517271c7df6672fb4b0117b0cd
Sangyups <feels4637@gmail.com>
2024-12-08 14:43:27 +0900

    chore: shell script 띄어쓰기 수정

--- git_push.sh
+++ git_push.sh
@@ -10,7 +10,7 @@ ask_and_confirm() {
         read -r -p "$question " response
 
         # Confirm the answer
-        read -r -p "'$response' 가 맞습니까? ([y]/n) " confirm
+        read -r -p "'$response' (이)가 맞습니까? ([y]/n) " confirm
         confirm=${confirm:-y}
 
         if [[ "$confirm" =~ ^[Yy]$ ]]; then
@@ -21,9 +21,9 @@ ask_and_confirm() {
     echo "$response"
 }
 
-prob=$(ask_and_confirm "문제 정보를 입력해주세요: ")
-lang=$(ask_and_confirm "언어 종류를 입력해주세요: ")
-site=$(ask_and_confirm "출처 사이트를 입력해주세요: ")
+prob=$(ask_and_confirm "문제 정보를 입력해주세요:")
+lang=$(ask_and_confirm "언어 종류를 입력해주세요:")
+site=$(ask_and_confirm "출처 사이트를 입력해주세요:")
 
 mkdir -p ./src/${site} && cp ./main.${lang} ./src/${site}/${prob}.${lang}
 echo "src/${site}/${prob}.${lang}에 저장되었습니다."
